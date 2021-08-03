b86e30cb8aed45602c3e285e9e2bf5aae086865e
Aaron <aaron.denhond@gmail.com>
2021-08-03 09:59:51 +0200

    playing around with routing and mvc structure in Symfony

---
+++ src/Controller/LearningController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LearningController extends AbstractController
+{
+    #[Route('/learning', name: 'learning')]
+    public function index(): Response
+    { 
+
+
+        return $this->render('learning/index.html.twig', [
+            'controller_name' => 'LearningController',
+        ]);
+    }
+
+    #[Route('/homepage', name: 'homepage')]
+    public function AboutMe(): Response
+    { 
+
+        
+        return $this->render('homepage/index.html.twig', [
+            'controller_name' => 'LearningController',
+        ]);
+    }
+}
